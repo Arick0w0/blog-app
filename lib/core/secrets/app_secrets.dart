32406a4b1c5a3575b112a67b58a49fa038d4d03f
master007mix <master007mix@gmail.com>
2024-07-27 23:26:28 +0700

    Update .gitignore to ignore app secrets

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabase = 'https://vlidvlakbwirwhcinoki.supabase.co';
-  static const supabaseAnnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsaWR2bGFrYndpcndoY2lub2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3MjQ0MzYsImV4cCI6MjAzNzMwMDQzNn0.zvOQg58e0G1zhARvPE2FO_A3BoMedFXYhRnHsb-8Mj0';
-}
